0af0bc19d6bd732fd14802e39a1821bb18c40360
Kelly Lo <kellylo@Kellys-MacBook-Pro.local>
2022-03-24 23:07:18 -0700

    first commit

---
+++ lo.kelly/parts/navbar.php
@@ -0,0 +1,15 @@
+<header class="navbar">
+    <div class="container display-flex">
+        <div class="flex-none">
+            <img src="img/logo_white.png" alt="logo">
+        </div>
+        <div class="flex-stretch"></div>
+        <nav class="nav nav-flex flex-none">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="product_list.php">Store</a></li>
+                <li><a href="#">Link 3</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
