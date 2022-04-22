3778772f27b7c09e16dce5a75e763d0e4adffdc5
mahsa.m <99348671+mahsamirchenari@users.noreply.github.com>
2022-04-22 15:02:03 -0500

    Add files via upload

---
+++ mirchenari.mahsa/lib/php/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+function MYSQLIAuth(){
+	return[
+ "localhost",   //mysql host 
+"mahsas",  //  mysql user name
+"mahsas",  // mysql user password
+"mahsas"   // mysql database name
+	];
+}
