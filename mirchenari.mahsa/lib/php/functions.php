f59095715996c5f6f2930b74e4f5a40b438dc2d9
mahsa.m <99348671+mahsamirchenari@users.noreply.github.com>
2022-04-22 15:02:48 -0500

    Update functions.php

--- mirchenari.mahsa/lib/php/functions.php
+++ mirchenari.mahsa/lib/php/functions.php
@@ -1,9 +1,8 @@
 <?php
 
- function print_p($v) {
-        echo"<pre>",print_r($v),"</pre>";
-    }
-
+function print_p($v) {
+   echo "<pre>",print_r($v),"</pre>";
+}
 
     function file_get_json($filename){
 
@@ -11,3 +10,25 @@
        return json_decode($file);
 
     }
+
+
+
+
+    include_once "auth.php";
+function makeConn(){
+   $conn = new mysqli(...MYSQLIAuth());
+   if($conn->connect_errno) die($conn->connect_error);
+   $conn->set_charset('utf8');
+   return $conn;
+}
+
+function makeQuery($conn,$qry){
+   $result = $conn->query($qry);
+   if($conn->errno) die($conn->error);
+   $a = [];
+   while ($row = $result->fetch_object()) {
+      $a[] = $row;
+   }
+
+   return $a;
+}
