5471d58de6122f840aae34a4b9b7b4f7e8bef307
mahsa.m <99348671+mahsamirchenari@users.noreply.github.com>
2022-04-30 11:08:38 -0500

    Update navbar.php

--- mirchenari.mahsa/parts/navbar.php
+++ mirchenari.mahsa/parts/navbar.php
@@ -1,3 +1,10 @@
+=	<?php
+
+
+	include_once "lib/php/functions.php";
+	?>
+
+
 	<header class="navbar">
 		<div class="container display-flex">
 			<div class="flex-none">
@@ -6,11 +13,16 @@
 			<div class="flex-stretch"></div>
 			<nav class="nav nav-flex flex-none">
 				<ul>
-					<li><a href="home.php">HOME</a></li>
+					<li><a href="index.php">HOME</a></li>
 					<li><a href="product_list.php">PRODUCTS</a></li>
 					<li><a href="about.php">ABOUT</a></li>
 				    <li><a href="contact.php">CONTACT US</a></li>
-				      <li><a href="login.php">LOGIN</a></li>
+				      <li><a href="product_cart.php">
+				      	<span>CART</span>
+                     <span class="badge"><?= makeCartBadge(); ?></span>
+
+
+				      </a></li>
 				</ul>
 			</nav>
 		</div>
