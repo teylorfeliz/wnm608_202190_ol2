fb86b63695d0de6bdcb966cd7425a58606d8b4f4
mahsa.m <99348671+mahsamirchenari@users.noreply.github.com>
2022-05-05 18:19:52 -0500

    Add files via upload

--- mirchenari.mahsa/product_list.php
+++ mirchenari.mahsa/product_list.php
@@ -1,43 +1,82 @@
-<!DOCTYPE html>
+<?php
+
+    include_once "lib/php/functions.php";
+     include_once "parts/templates.php";
+
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     
     <title>Product List</title>
-    <?php include "parts/meta.php"; ?>
-<?php include "parts/navbar.php"; ?>
+
+   
+   <?php include "parts/meta.php"; ?>
+  <?php include "parts/navbar.php"; ?>
+
+    <script src="lib/js/functions.js"></script>
+    <script src="js/templates.js"></script>
+    <script src="js/product_list.js"></script>
+
+
     
 </head>
 <body>
 
+
+
 <div class="container">
-   
-            <h2>PRODUCT LIST</h2>
-            <!-- <ul>
-                <li><a href="product_item.php?id=1">Product One</a></li>
-                <li><a href="product_item.php?id=2">Product Two</a></li>
-                <li><a href="product_item.php?id=3">Product Three</a></li>
-                <li><a href="product_item.php?id=4">Product Four</a></li>
-            </ul> -->
+       <h2>PRODUCT LIST</h2>
 
-            <?php
-            include_once "lib/php/functions.php";
-              include_once "parts/templates.php";
-         
-            $result = makeQuery(makeConn(), "
-                SELECT *
-                FROM `products`
-                ORDER BY `price`
+         <div class="form-control">
+                <form class="hotdog " id="product-search">
+                    <input type="search" placeholder="Search Products">
+                </form>
+            </div>
+
+
+                   
+         <div class="form-control">
+                <div class="card soft">
+                        <div class="display-flex">
+                
+                            <div class="flex-stretch display-flex">
+                              
+
+                            <div class="flex-none">
+                                <button data-filter="category" data-value="" type="button" class="form-button">All</button>
+                            </div>
+                          <div class="flex-none">
+                                <button data-filter="category" data-value="women" type="button" class="form-button">WOMEN</button>
+                            </div>
+                              <div class="flex-none">
+                                <button data-filter="category" data-value="men" type="button" class="form-button">MEN</button>
+                            </div>
+                            </div>  
+ 
+ 
+               <div class="flex-none">
+                            <div class="form-select">
+                                <select class="js-sort">
+                                    <option value="1">PRICE LOW TO HIGH </option>
+                                    <option value="2">PRICE HIGH TO LOW</option>
+                                 
+                                </select>
+                            </div>
+                        </div>
+                        </div>
+
+                        </div>
               
-            ");
-            // print_p($result);
-            echo "<div class= 'productlist grid gap'>", array_reduce($result,'productListTemplate'), "</div>";
-            ?>
+                               </div>
+                        
 
-
+            
+            <div class='productlist grid gap'></div>
     </div>
+    
 
-    </div>
 <?php include "parts/footer.php"; ?>
 
 </body>
