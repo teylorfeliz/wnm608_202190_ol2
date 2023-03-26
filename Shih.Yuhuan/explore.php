a4db7807e422e121fe2f36b573cbbd87a9645e19
dixxit10 <114282161+dixxit10@users.noreply.github.com>
2023-03-26 15:31:52 -0700

    Add files via upload

---
+++ Shih.Yuhuan/explore.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+	
+	<div class="container">
+		<div class="card soft">
+			<h2>Explore</h2>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ Shih.Yuhuan/product.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+	
+	<div class="container">
+		<div class="card flat">
+			<h2>Product</h2>
+
+
+			 <div class="grid gap">
+                <div class="col-xs-12 col-md-3">
+                    <figure class="figure">
+                        <a href="product_item.php?id=1"><img src="img/backpack.jpg" style="width: 400; height: 400;" alt="backpack"></a>
+                        <figcaption>Backpack</figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                    <figure class="figure">
+                        <a href="product_item.php?id=2"><img src="img/campfurniture.jpg" style="width: 400; height: 400;" alt="backpack"></a>
+                        <figcaption>Camp Furniture</figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                    <figure class="figure">
+                        <a href="product_item.php?id=3"><img src="img/clothing.jpg" style="width: 400; height: 400;" alt="Clothing"></a>
+                        <figcaption>Clothing</figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                    <figure class="figure">
+                        <a href="product_item.php?id=4"><img src="img/footwear.jpg" style="width: 400; height: 400;" alt="Footwear"></a>
+                        <figcaption>Footwear</figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                    <figure class="figure">
+                        <a href="product_item.php?id=5"><img src="img/tent.jpg" style="width: 400; height: 400;" alt="Tents"></a>
+                        <figcaption>Tents</figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                	<figure class="figure">
+                    	<a href="product_item.php?id=6"><img src="img/lighting.jpg" style="width: 400; height: 400;" alt="Lighting"></a>
+                        <figcaption>Lighting</figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                	<figure class="figure">
+                    	<a href="product_item.php?id=7"><img src="img/sleepingbag.jpg" style="width: 400; height: 400;" alt="Sleepingbag"></a>
+                        <figcaption>Sleepingbag</figcaption>
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-3">
+                	<figure class="figure">
+                    	<a href="product_item.php?id=8"><img src="img/padsandhammocks.jpg" style="width: 400; height: 400;" alt="Pads & Hammocks"></a>
+                        <figcaption>Pads & Hammocks</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+	</div>
+
+
+</body>
+</html>
