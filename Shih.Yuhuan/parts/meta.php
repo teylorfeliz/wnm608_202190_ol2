31b734327bea43f35db189059d1e9ad9e246a4d8
dixxit10 <114282161+dixxit10@users.noreply.github.com>
2023-03-26 15:32:12 -0700

    Add files via upload

---
+++ Shih.Yuhuan/parts/meta.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" type="text/css" href="css/storetheme.css">
+	<link href = 'https://fonts.googleapis.com/css2?family=Montserrat&family=Playfair+Display&display=swap'  rel = 'stylesheet'  type = 'text/css' >  
---
+++ Shih.Yuhuan/parts/navbar.php
@@ -0,0 +1,22 @@
+
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<a href="index.html"><img src="img/logo.png" style="width:85px; padding:15%"></a>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+				<ul>
+					<li><a href="about.php">About</a></li>
+					<li><a href="product.php">Product</a></li>
+					<li><a href="deals.php">Deals</a></li>
+					<li><a href="explore.php">Explore</a></li>
+					<li><a href="login.php">Login</a></li>
+					<li><a href="cart.php">Cart</a></li>
+				</ul>
+				
+			</nav>
+			
+		</div>
+	</header>
+	
