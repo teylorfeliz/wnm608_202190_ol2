3bd4ff9676a71058a67cfb0df2031731ab0b4435
jyoung7014 <125100460+jyoung7014@users.noreply.github.com>
2023-03-26 21:34:24 -0700

    Add files via upload

---
+++ park.jaeyoung/product_list.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Product List</title>
+
+    <?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+    <?php include "parts/navbar.php"; ?>
+
+    <div class="container">
+    	<div class="card soft">
+    		<h2>Product List</h2>
+
+    		<!--ul>li*4>a[href="product_item.php"]>{product $}-->
+    		<ul>
+    			<li><a href="product_item.php?id=1">product 1</a></li>
+    			<li><a href="product_item.php?id=2">product 2</a></li>
+    			<li><a href="product_item.php?id=3">product 3</a></li>
+    			<li><a href="product_item.php?id=4">product 4</a></li>
+    		</ul>
+    	</div>
+    </div>
+
+</body>
+</html>
