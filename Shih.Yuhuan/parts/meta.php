9f8f56d4280bed16ebd5ccb8f66998eccf89be73
dixxit10 <114282161+dixxit10@users.noreply.github.com>
2023-04-02 04:51:54 -0700

    Add files via upload

--- Shih.Yuhuan/parts/meta.php
+++ Shih.Yuhuan/parts/meta.php
@@ -1,5 +1,8 @@
 <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<base href="http://joyshih.com/wnm608/Shih.Yuhuan/">
+
 	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
 	<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
 	<link rel="stylesheet" type="text/css" href="css/storetheme.css">
--- Shih.Yuhuan/parts/navbar.php
+++ Shih.Yuhuan/parts/navbar.php
@@ -2,7 +2,7 @@
 <header class="navbar">
 		<div class="container display-flex">
 			<div class="flex-none">
-				<a href="index.html"><img src="img/logo.png" style="width:85px; padding:15%"></a>
+				<a href="index.php"><img src="img/logo.png" style="width:85px; padding:15%"></a>
 			</div>
 			<div class="flex-stretch"></div>
 			<nav class="nav nav-flex flex-none">
