a57e73de240bf39f3c6179244fadd40c08a4aa41
dixxit10 <114282161+dixxit10@users.noreply.github.com>
2023-04-16 14:50:26 -0700

    Add files via upload

--- Shih.Yuhuan/admin/users.php
+++ Shih.Yuhuan/admin/users.php
@@ -7,95 +7,136 @@ $users = file_get_json("../data/users.json");
 $save = "Save Changes";
 
 
-
-
-function showUserPage($user) {
-
-
 $save = $GLOBALS['save'];
-if($user->name==""){
+$users = $GLOBALS['users'];
+
+
+if($user->name==""){    
     $user_name = '<div class="form-control">
         <label class="form-label">Name</label>
         <input required=ture type="text" value="" name="name" class="form-inputs">
         </div>';
+
 } else {
     $user_name = '<h2>'.$user->name.'</h2>'.'<input required=ture style="display:none" name="name" type="text" value="'.$user->name.'">';
 }
 
-$classes = implode(", ", $user->classes);
-    // print_p($user);
-    // herrdoc
 
-echo <<<HTML
-<nav class="nav nav-crumbs">
-    <ul>
-        <li><a href="admin/users.php">Back</a></li>
-    </ul>
-</nav>
-<div>
 
+    if(isset($_GET['action'])) {
+
+        switch($_GET['action']) {
+            case "Update":
+                // print_p($users);
+                // $users[$_GET['id']]->name = $_POST['name'];
+                $users[$_GET['id']]->type = $_POST['type'];
+                $users[$_GET['id']]->email = $_POST['email'];
+                $users[$_GET['id']]->classes = preg_split("/[\s,]+/",$_POST['classes']);
+                
+                file_put_json("../data/users.json", $users);
+                header("location:{$_SERVER['PHP_SELF']}?id={$_GET['id']}");
+                break;
+
+            case "delete":
+                array_splice($users,$_GET['id'],1);
+                file_put_json("../data/users.json", $users);
+                header("location:{$_SERVER['PHP_SELF']}");
+                break;
+            
+           
+        }
+    }
+
+
+
+  
+    // if(isset($_POST['delete'])){
+
+    //     unset($users[$_GET['id']]);
+    //     header("location: http://joyshih.com/wnm608/Shih.Yuhuan/admin/users.php");
+    //     file_put_json("../data/users.json", array_values($users));
+
+    // };
     
 
 
-    <form action="" method="post" name="userData">
+function showUserPage($user) {
+
+$id = $_GET['id'];
+$addoredit = $id == "new" ? "Add" : "Edit";
+$createorupdate = $id == "new" ? "Create" : "Update";
+$classes = implode(", ", $user->classes);
+
+$display = <<<HTML
+    <div><h2>{$user->name}</h2>
+    
+        <div>
+            <strong>Type</strong>
+            <span>$user->type</span>
+        </div>
+        <div>
+            <strong>Email</strong>
+            <span>$user->email</span>
+        </div>
+        <div>
+            <strong>Classes</strong>
+            <span>$classes</span>
+        </div>
+    </div>
+    HTML;
+
+    $form = <<<HTML
+
+    <form action="{$_SERVER['PHP_SELF']}?id=$id&action=$createorupdate" method="post" name="userData">
     $user_name
         <div class="form-control">
-            <label class="form-label">Type</label>
-            <input type="text" required=ture value="$user->type" name="type" class="form-inputs">
-        </div>
-            <div class="form-control">
-            <label class="form-label">Email</label>
-            <input type="email" required=ture value="$user->email" name="email" class="form-inputs">
+            <label class="form-label" for="user-type">Name</label>
+            <input type="text" required=ture value="$user->name" name="name" id="name" class="form-inputs" placeholder="Enter your name">
         </div>
         <div class="form-control">
-            <label class="form-label">Class</label>
-            <input type="text" required=ture value="$classes" name="classes" class="form-inputs">
+            <label class="form-label" for="user-type">Type</label>
+            <input type="text" required=ture value="$user->type" name="type" id="type" class="form-inputs" placeholder="Enter your type">
+        </div>
+            <div class="form-control">
+            <label class="form-label" for="user-email">Email</label>
+            <input type="email" required=ture value="$user->email" name="email" id="email" class="form-inputs" placeholder="Enter your email">
+        </div>
+        <div class="form-control">
+            <label class="form-label" for="user-classes">Class</label>
+            <input type="text" required=ture value="$classes" name="classes" id="classes" class="form-inputs" placeholder="Enter your classes">
         </div>
 
         <div class="form-control">
-            <input style="background-color: #053426" type="submit" name="save" class="form-button card dark" value="$save">
+            <input style="background-color: #053426" type="submit" name="save" class="form-button card dark" value="$createorupdate">
         </div>
 
         <div class="form-control">
             <input style="background-color: transparent; color: #053426 ;" type="submit" name="delete" class="form-button card hard" value="Delete">
-        </div>
-
-        <!--<input type="submit" value="submit" name="submit">-->
+        </div>      
     </form>
-
-
-</div>
 HTML;
+
+$output = $id == "new" ? $form :
+    "<div class='grid gap'>
+    <div class='col-xs-12 col-md-7'>$display</div>
+    <div class='col-xs-12 col-md-5'>$form</div>
+    </div>";
+
+$delete = $id == "new" ? "" : "<a href='{$_SERVER['PHP_SELF']}?id=$id&action=delete'>Delete</a>";
+
+
+echo <<<HTML
+<nav class="display-flex">
+    <div class="flex-stretch"><a href="{$_SERVER['PHP_SELF']}">Back</a></div>
+    <div class="flex-none">$delete</div>
+</nav>
+
+$output
+HTML;
+
 }
 
 
- ?>
-
-<?php 
-    
-
-    if(isset($_POST['save'])){
-        if($_POST['name']){
-
-            $users[$_GET['id']]->name = $_POST['name'];
-            $users[$_GET['id']]->type = $_POST['type'];
-            $users[$_GET['id']]->email = $_POST['email'];
-            $users[$_GET['id']]->classes = preg_split("/[\s,]+/",$_POST['classes']);
-            
-            file_put_json("../data/users.json", $users);
-            $save = "sucessfully saved!";
-            header("Refresh:1;");
-        }
-
-    };
-
-    if(isset($_POST['delete'])){
-
-        unset($users[$_GET['id']]);
-        header("location: http://joyshih.com/wnm608/Shih.Yuhuan/admin/users.php");
-        file_put_json("../data/users.json", array_values($users));
-
-    };
 
  ?>
 
@@ -119,7 +160,8 @@ HTML;
           <div class="flex-stretch"></div>
           <nav class="nav nav-flex flex-none">
               <ul>
-                  <li><a href="admin/users.php">User List</a></li>
+                  <li><a href="<?= $_SERVER['PHP_SELF'] ?>">User List</a></li>
+                  <li><a href="<?= $_SERVER['PHP_SELF'] ?>?id=new">Add New User</a></li>
               </ul>
           </nav>
         
@@ -130,20 +172,37 @@ HTML;
 
             <?php
                 if(isset($_GET['id'])) {
-                    if($users[$_GET['id']]){
-                        showUserPage($users[$_GET['id']]);
-                    } else {
+                    if($_GET['id']=="new"){
+
                         $new_user = (object)[
                             "name"=>"",
                             "type"=>"",
                             "email"=>"",
                             "classes"=>[]
                         ];
+
                         showUserPage($new_user);
-                        // print_p($new_user->name);
-                        // echo $new_user->name;
+
+                        if(isset($_POST['save'])){
+                            $id = count($users);
+                            $users[$id]->name = $_POST['name'];
+                            $users[$id]->type = $_POST['type'];
+                            $users[$id]->email = $_POST['email'];
+                            $users[$id]->classes = preg_split("/[\s,]+/",$_POST['classes']);
+                            file_put_json("../data/users.json", $users);
+                            header("location:{$_SERVER['PHP_SELF']}?id=$id");
+                        }
+
+
+                    } else if ($user = $users[$_GET['id']]){
+                        
+                        showUserPage($user);
+
+               
+                        
+                
                     }
-                    
+
                 } else {
 
             ?>
