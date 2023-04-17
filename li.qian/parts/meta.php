afce7d06d77660bf657d3efa0799986dff0e8515
clover li <clover@clovers-MacBook-Pro.local>
2023-04-16 19:30:20 -0700

    m9

--- li.qian/parts/meta.php
+++ li.qian/parts/meta.php
@@ -1,5 +1,6 @@
 	<base href="http://qianlimeow.com/AAU/wnm608/li.qian/">
 	
-	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<!-- <link rel="stylesheet" href="lib/css/styleguide.css"> -->
 	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="lib/css/projectstyle.css">
 	<link rel="stylesheet" href="css/storetheme.css">
