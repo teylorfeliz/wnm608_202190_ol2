c428ca473f7052c3d6dd7fad8aabf009ce8b45a9
李語彤 <yutunglee@liyutongdeMBP.attlocal.net>
2023-05-16 08:49:40 -0700

    014

--- yutung.lee/README.md
+++ yutung.lee/README.md
@@ -4,6 +4,11 @@
 - www.yutunglee.com	
 - www.yutunglee.com/AAU/wnm608_202190_ol2/yutung.lee
 - www.yutunglee.com/AAU/wnm608_202190_ol2/yutung.lee/styleguide
-- http://www.yutunglee.com/Development/home/yvn86jesdiy5/public_html/Development/wnm608_202190_ol2/yutung.lee/notes/reading_data.php
 
+
+- http://www.yutunglee.com/Development/home/yvn86jesdiy5/public_html/Development/wnm608_202190_ol2/yutung.lee/admin
+
+## Extra Links
 - http://www.yutunglee.com/Development/home/yvn86jesdiy5/public_html/Development/wnm608_202190_ol2/yutung.lee/admin/users.php
+- http://www.yutunglee.com/Development/home/yvn86jesdiy5/public_html/Development/wnm608_202190_ol2/yutung.lee/notes
+- http://www.yutunglee.com/Development/home/yvn86jesdiy5/public_html/Development/wnm608_202190_ol2/yutung.lee/notes/reading_data.php
--- yutung.lee/data/products.sql
+++ yutung.lee/data/products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost:3306
--- 產生時間： 2023 年 05 月 15 日 16:07
+-- 產生時間： 2023 年 05 月 16 日 05:17
 -- 伺服器版本： 5.7.41-cll-lve
 -- PHP 版本： 7.4.30
 
@@ -38,26 +38,27 @@ CREATE TABLE `products` (
   `data_modify` datetime NOT NULL,
   `description` text NOT NULL,
   `thumbnail` varchar(128) NOT NULL,
-  `images` varchar(256) NOT NULL
+  `images` varchar(256) NOT NULL,
+  `quantity` tinyint(200) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- 傾印資料表的資料 `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `url`, `price`, `category`, `data_create`, `data_modify`, `description`, `thumbnail`, `images`) VALUES
-(1, 'Hill\'s: Chicken Recipe', '', '61.99', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'dsvcscscc', 'catfood_thumb.png', 'catfood02_thumb.png,catfood03_thumb.png'),
-(2, 'Hill\'s: Light Chicken Recipe', '', '59.99', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'davdvdvsvz', 'catfood01_thumb.png', 'catfood_thumb.png,catfood03_thumb.png'),
-(3, 'Purina: Salmon Recipe', '', '17.98', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'csdcscv sv ', 'catfood02_thumb.png', 'catfood02_thumb.png'),
-(4, 'So Thresh : oder control Cat litter', '', '13.99', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'z xc dvs', 'catlitter_thumb.png', 'catlitter_thumb.png'),
-(5, 'Tidy Cats : Multi Cat litter', '', '20.22', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', ' f dsvsccasc', 'catlitter01_thumb.png', 'catlitter01_thumb.png'),
-(6, 'Tidy Cats : Max 4 in 1 Cat litter', '', '20.22', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'vfsbdvsvcz', 'catlitter02_thumb.png', 'catlitter02_thumb.png'),
-(7, 'YOULY : Valentine\'s Day Cat Scratcher', '', '5.00', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', ' d d. s zxzdc ', 'cattoy_thumb.png', 'cattoy_thumb.png'),
-(8, 'YOULY : Valentine\'s Day Cat toy', '', '4.50', 'cat toys', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'd z. sdcsaca', 'cattoy01_thumb.png', 'cattoy01_thumb.png'),
-(9, 'YOULY : Valentine\'s Day fish cat toy', '', '3.50', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', ' fs vscac scsc', 'cattoy02_thumb.png', 'cattoy02_thumb.png'),
-(10, 'WholeHearted : Rice & Chicken Recipe', '', '54.99', 'Dog', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'dvvscscs sc', 'dogfood_thumb.png', 'dogfood_thumb.png'),
-(11, 'Purina : : Rice & Chicken Recipe', '', '59.88', 'Dog', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'sbvdfvssvcsdc', 'dogfood01_thumb.png', 'dogfood01_thumb.png'),
-(12, 'Purina : : Rice & Lamb Recipe', '', '59.88', 'Dog', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'vxv cscxczc', 'dogfood02_thumb.png', 'dogfood02_thumb.png');
+INSERT INTO `products` (`id`, `name`, `url`, `price`, `category`, `data_create`, `data_modify`, `description`, `thumbnail`, `images`, `quantity`) VALUES
+(1, 'Hill\'s: Chicken Recipe', '', '61.99', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'dsvcscscc', 'catfood_thumb.png', 'catfood02_thumb.png,catfood03_thumb.png', 60),
+(2, 'Hill\'s: Light Chicken Recipe', '', '59.99', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'davdvdvsvz', 'catfood01_thumb.png', 'catfood_thumb.png,catfood03_thumb.png', 70),
+(3, 'Purina: Salmon Recipe', '', '17.98', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'csdcscv sv ', 'catfood02_thumb.png', 'catfood02_thumb.png', 50),
+(4, 'So Thresh : oder control Cat litter', '', '13.99', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'z xc dvs', 'catlitter_thumb.png', 'catlitter_thumb.png', 90),
+(5, 'Tidy Cats : Multi Cat litter', '', '20.22', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', ' f dsvsccasc', 'catlitter01_thumb.png', 'catlitter01_thumb.png', 100),
+(6, 'Tidy Cats : Max 4 in 1 Cat litter', '', '20.22', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'vfsbdvsvcz', 'catlitter02_thumb.png', 'catlitter02_thumb.png', 40),
+(7, 'YOULY : Valentine\'s Day Cat Scratcher', '', '5.00', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', ' d d. s zxzdc ', 'cattoy_thumb.png', 'cattoy_thumb.png', 60),
+(8, 'YOULY : Valentine\'s Day Cat toy', '', '4.50', 'cat toys', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'd z. sdcsaca', 'cattoy01_thumb.png', 'cattoy01_thumb.png', 70),
+(9, 'YOULY : Valentine\'s Day fish cat toy', '', '3.50', 'Cat', '2023-05-15 16:07:02', '2023-05-15 16:07:02', ' fs vscac scsc', 'cattoy02_thumb.png', 'cattoy02_thumb.png', 120),
+(10, 'WholeHearted : Rice & Chicken Recipe', '', '54.99', 'Dog', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'dvvscscs sc', 'dogfood_thumb.png', 'dogfood_thumb.png', 100),
+(11, 'Purina : : Rice & Chicken Recipe', '', '59.88', 'Dog', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'sbvdfvssvcsdc', 'dogfood01_thumb.png', 'dogfood01_thumb.png', 90),
+(12, 'Purina : : Rice & Lamb Recipe', '', '59.88', 'Dog', '2023-05-15 16:07:02', '2023-05-15 16:07:02', 'vxv cscxczc', 'dogfood02_thumb.png', 'dogfood02_thumb.png', 80);
 
 --
 -- 已傾印資料表的索引
