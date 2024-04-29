e1919a6763f17e905490520e014670aa73aed38c
sfiiAAU <125102642+sfiiAAU@users.noreply.github.com>
2024-04-29 00:45:32 -0700

    Module 10

--- sfdesigns/aau/ixd608/foster.stephen/product_item.php
+++ sfdesigns/aau/ixd608/foster.stephen/product_item.php
@@ -1,22 +1,79 @@
-<!DOCTYPE html>
+<?php
+
+include_once "lib/php/functions.php";
+
+$products = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id`=".$_GET['id'])[0];
+
+$images = explode(",", $products->images);
+
+$image_elements = array_reduce($images,function($r,$o){
+	return $r."<img src='/img/$o'>";
+});
+
+// print_p($product);
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Product Item</title>
+	<title>Product Item</title>	
 
 	<?php include "parts/meta.php"; ?>
+
+	<script src="js/product_thumbs.js"></script>
 </head>
 <body>
 
 	<?php include "parts/navbar.php"; ?>
 
-<div class="containter">
-	<div class="card soft">
-		<h2>Product Item</h2>
+	<div class="container">
+		<div class="grid gap">
+			<div class="col-xs-12 col-md-7">
+				<div class="card soft">
+					<div class="images-main">
+						<img src="img/<?= $products->images ?>">
+					</div>
+					<div class="images-thumbs">
+						<?= $image_elements ?>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-5">
+				<div class="card soft flat">
+					<div class="card-section">
+						<h2 class="product-name"><?= $products->name ?></h2>
+						<div class="product-price">&dollar;<?= $products->price ?></div>
+					</div>
 
-		<p>This is item # <?= $_GET['id'] ?></p>
+					<div class="card-section">
+						<label for="product-amount" class="form-label">Amount</label>
+						<div class="form-select" id="product-amount">
+							<select>
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5</option>
+								<option>6</option>
+								<option>7</option>
+								<option>8</option>
+								<option>9</option>
+								<option>10</option>
+							</select>
+						</div>
+					</div>
+
+					<div class="card-section">
+						<a href="product_added_to_cart.php?id=<? $product->id ?>" class="form-button">Add To Cart</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="card soft dark">
+			<p><?= $product->description ?></p>
+		</div>
 	</div>
-</div>
 
 </body>
 </html>
